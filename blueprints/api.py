08da0d5aad3efe0f581346f8a6c9f1c8662ef57f
ClaudioRapisardi01 <c.rapisardi@capecchispa.eu>
2025-05-05 12:44:45 +0200

    modifica api

--- blueprints/api.py
+++ blueprints/api.py
@@ -4,6 +4,16 @@ from flask import Flask, render_template, request, redirect, url_for, flash, Blu
 api_bp = Blueprint('api', __name__, url_prefix='/api')
 
 
-@api_bp.route('/')
-def index():
-    return render_template('index.html')
+@api_bp.route('/sendInfo/')
+def sendInfo():
+    print("info")
+
+@api_bp.route('/sendInfoFull/')
+def sendInfoFull():
+    print("info full")
+
+@api_bp.route('/sendSpeedTest/')
+def sendSpeedTest():
+    print("speed Test")
+
+
